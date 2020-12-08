e23af7b3149a33405c9d419480c753a06d2ae761
faker007 <rlaqjadud195@naver.com>
2020-12-08 23:26:02 +0900

    [Modify] Slightly change2 for working ;)

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ const myPeer = new Peer(undefined, {
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 const peers = {};
-window.navigator.mediaDevices
+navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
